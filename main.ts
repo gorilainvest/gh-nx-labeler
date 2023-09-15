bb633ddfdcfc33d73ce9ef5022599801642995f4
Pedro Morello Abbud <abbud07@gmail.com>
2023-09-15 17:36:45 -0300

    fix: simplify setup

--- main.ts
+++ main.ts
@@ -1,50 +1,75 @@
-import * as github from '@actions/github';
-import * as core from '@actions/core';
-import nx from '@nx/devkit';
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import * as github from '@actions/github'
+import nx from '@nx/devkit'
+import { exec } from 'child_process'
+import { promisify } from 'util'
 
-const execAsync = promisify(exec);
+type LabelPrefixDefinition = {
+  color: string
+  description: string
+}
+
+type Octokit = ReturnType<typeof github.getOctokit>
+
+type EnvironmentVaribles = {
+  nxHead: string
+  nxBase: string
+  token: string
+  projectTypeAbbreviations: Record<string, string>
+  labelPrefix: Record<string, LabelPrefixDefinition>
+  allAffectedTag: string
+}
+
+const execAsync = promisify(exec)
+
+const defaultProjectTypeAbbreviations: Record<string, string> = {
+  "application": "app",
+  "library": "lib",
+  "language": "lang"
+};
+
+const defaultLabelPrefixDefinitions: Record<string, LabelPrefixDefinition> = {
+  "app": {
+    "color": "D4C5F9",
+    "description": "Pull request affected the application"
+  },
+  "lang": {
+    "color": "C2E0C6",
+    "description": "Projects of this language were affected by the pull request"
+  },
+  "lib": {
+    "color": "BFD4F2",
+    "description": "Pull request affected the library"
+  }
+};
 
 async function getAffectedProjects(
   nxBase: string,
   nxHead: string
 ): Promise<string[]> {
+  console.log('Getting affected projects names')
   const { stdout } = await execAsync(
     `yarn nx show projects --affected --base=${nxBase} ---head=${nxHead}`
-  );
-  return stdout.split('\n').filter((line) => line.length > 0);
+  )
+  return stdout.split('\n').filter(line => line.length > 0)
 }
 
-const defaultLabelPrefixDefinitions = {
-  app: {
-    color: 'D4C5F9',
-    description: 'Pull request affected the application',
-  },
-  lang: {
-    color: 'C2E0C6',
-    description: 'Projects of this language were affected by the pull request',
-  },
-  lib: { color: 'BFD4F2', description: 'Pull request affected the library' },
-};
-
-const defaultProjectTypeAbbreviations = {
-  application: 'app',
-  library: 'lib',
-  language: 'lang',
-};
-
-function getEnvironmentVariables() {
-  const token = core.getInput("GITHUB_TOKEN")
+function getEnvironmentVariables(): EnvironmentVaribles {
+  const token = process.env.GITHUB_TOKEN
   if (!token) {
-    throw Error('GITHUB_TOKEN is required');
+    throw Error('GITHUB_TOKEN is required')
   }
 
-  const allAffectedTag = core.getInput("ALL_AFFECTED_TAG")
-  const labelPrefix = JSON.parse(core.getInput("LABEL_PREFIX_DEFINITIONS"))
-  const projectTypeAbbreviations = JSON.parse(core.getInput("PROJECT_TYPE_ABBREVIATIONS"))
-  const nxHead = core.getInput("NX_HEAD")
-  const nxBase = core.getInput("NX_BASE")
+  const allAffectedTag = process.env.ALL_AFFECTED_TAG
+  const labelPrefix = process.env.LABEL_PREFIX_DEFINITIONS 
+  ? JSON.parse(process.env.LABEL_PREFIX_DEFINITIONS) as Record<string, LabelPrefixDefinition>
+    : defaultLabelPrefixDefinitions
+
+  const projectTypeAbbreviations = process.env.PROJECT_TYPE_ABBREVIATIONS
+    ? JSON.parse(process.env.PROJECT_TYPE_ABBREVIATIONS) as Record<string, string>
+    : defaultProjectTypeAbbreviations
+
+  const nxHead = process.env.NX_HEAD
+  const nxBase = process.env.NX_BASE
 
   return {
     nxHead,
@@ -52,100 +77,118 @@ function getEnvironmentVariables() {
     token,
     projectTypeAbbreviations,
     labelPrefix,
-    allAffectedTag,
-  };
+    allAffectedTag
+  }
 }
 
 async function collectAffectedTags(
   allAffectedTag: string,
   nxBase: string,
-  nxHead: string
+  nxHead: string,
+  projectTypeAbbreviations: Record<string, string>
 ): Promise<Set<string>> {
-  const tags = new Set<string>();
-  const projectGraph = await nx.createProjectGraphAsync();
-  const affected = await getAffectedProjects(nxBase, nxHead);
+  const tags = new Set<string>()
+  const projectGraph = await nx.createProjectGraphAsync()
+  const affected = await getAffectedProjects(nxBase, nxHead)
+  console.log('Affected projects: ', affected)
   const configurations =
-    nx.readProjectsConfigurationFromProjectGraph(projectGraph).projects;
+    nx.readProjectsConfigurationFromProjectGraph(projectGraph).projects
 
   if (affected.length === Object.keys(configurations).length) {
-    tags.add(allAffectedTag);
+    tags.add(allAffectedTag)
   } else {
     for (const project of affected) {
-      const config = configurations[project];
-      config.tags.forEach((tag) => {
-        const [tagPrefix, tagSuffix] = tag.split(':');
-        if (defaultProjectTypeAbbreviations[tagPrefix]) {
-          tags.add(`${tagPrefix}:${tagSuffix}`);
+      const config = configurations[project]
+      for (const tag of config.tags) {
+        const [tagPrefix, tagSuffix] = tag.split(':')
+        if (projectTypeAbbreviations[tagPrefix]) {
+          tags.add(`${tagPrefix}:${tagSuffix}`)
         } else {
-          tags.add(tag);
+          tags.add(tag)
         }
-      });
-      tags.add(
-        `${defaultProjectTypeAbbreviations[config.projectType]}:${config.name}`
-      );
+      }
+      tags.add(`${projectTypeAbbreviations[config.projectType]}:${config.name}`)
     }
   }
-  return tags;
+  return tags
 }
 
-async function fetchRepositoryLabels(octokit): Promise<Set<string>> {
-  const repositoryLabels = new Set<string>();
-  let hasMorePages = true;
-  let page = 1;
+async function fetchRepositoryLabels(octokit: Octokit): Promise<Set<string>> {
+  const repositoryLabels = new Set<string>()
+  let hasMorePages = true
+  let page = 1
 
   while (hasMorePages) {
     const result = await octokit.rest.issues.listLabelsForRepo({
       owner: 'gorilainvest',
       repo: 'securities',
       per_page: 100,
-      page,
-    });
-    result.data.map((label) => repositoryLabels.add(label.name));
-    hasMorePages = result.data.length === 100;
-    page++;
+      page
+    })
+    result.data.map(label => repositoryLabels.add(label.name))
+    hasMorePages = result.data.length === 100
+    page++
   }
 
-  return repositoryLabels;
+  return repositoryLabels
 }
 
 async function createMissingLabels(
-  octokit,
+  octokit: Octokit,
   tags: Set<string>,
-  existingLabels: Set<string>
-) {
+  existingLabels: Set<string>,
+  labelPrefix: Record<string, LabelPrefixDefinition>
+): Promise<void> {
   for (const tag of tags) {
     if (!existingLabels.has(tag)) {
-      const [tagPrefix] = tag.split(':');
-      if (defaultLabelPrefixDefinitions[tagPrefix]) {
-        console.log(`Creating custom definition for label ${tag}`);
+      const [tagPrefix] = tag.split(':')
+      if (labelPrefix[tagPrefix]) {
+        console.log(`Creating custom definition for label ${tag}`)
         await octokit.rest.issues.createLabel({
           owner: 'gorilainvest',
           repo: 'securities',
           name: tag,
-          color: defaultLabelPrefixDefinitions[tagPrefix].color,
-          description: defaultLabelPrefixDefinitions[tagPrefix].description,
-        });
+          color: labelPrefix[tagPrefix].color,
+          description: labelPrefix[tagPrefix].description
+        })
       }
     }
   }
 }
 
-export default async function main() {
-  const { nxBase, nxHead, token, allAffectedTag } = getEnvironmentVariables();
-  const octokit = github.getOctokit(token);
+export async function run(): Promise<void> {
+  const {
+    nxBase,
+    nxHead,
+    token,
+    allAffectedTag,
+    projectTypeAbbreviations,
+    labelPrefix
+  } = getEnvironmentVariables()
+  const octokit = github.getOctokit(token)
 
-  const affectedTags = await collectAffectedTags(allAffectedTag, nxBase, nxHead);
-  const repositoryLabels = await fetchRepositoryLabels(octokit);
+  const affectedTags = await collectAffectedTags(
+    allAffectedTag,
+    nxBase,
+    nxHead,
+    projectTypeAbbreviations
+  )
 
-  await createMissingLabels(octokit, affectedTags, repositoryLabels);
+    console.log(affectedTags)
+  const repositoryLabels = await fetchRepositoryLabels(octokit)
 
-  const { owner, repo, number } = github.context.issue;
+  await createMissingLabels(
+    octokit,
+    affectedTags,
+    repositoryLabels,
+    labelPrefix
+  )
+
+  const { owner, repo, number } = github.context.issue
   await octokit.rest.issues.addLabels({
     owner,
     repo,
     issue_number: number,
-    labels: Array.from(affectedTags),
-  });
+    labels: Array.from(affectedTags)
+  })
 }
-
-await main();
