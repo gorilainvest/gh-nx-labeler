cf0808d4d5b94f7c210d77cf347c28c34bb46a46
Pedro Morello Abbud <abbud07@gmail.com>
2023-09-15 13:58:53 -0300

    feat: add nx labeler exec

---
+++ dist/643.index.js
@@ -0,0 +1,44 @@
+exports.id = 643;
+exports.ids = [643];
+exports.modules = {
+
+/***/ 56215:
+/***/ ((module) => {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(() => {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = () => ([]);
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = 56215;
+module.exports = webpackEmptyAsyncContext;
+
+/***/ }),
+
+/***/ 70495:
+/***/ ((module) => {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(() => {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = () => ([]);
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = 70495;
+module.exports = webpackEmptyAsyncContext;
+
+/***/ })
+
+};
+;
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,159 @@
+import * as github from '@actions/github'
 import * as core from '@actions/core'
-import { wait } from './wait'
+import nx from '@nx/devkit'
+import { exec } from 'child_process'
+import { promisify } from 'util'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+type LabelPrefixDefinition = {
+  color: string
+  description: string
+}
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+type Octokit = ReturnType<typeof github.getOctokit>
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+const execAsync = promisify(exec)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+async function getAffectedProjects(
+  nxBase: string,
+  nxHead: string
+): Promise<string[]> {
+  const { stdout } = await execAsync(
+    `yarn nx show projects --affected --base=${nxBase} ---head=${nxHead}`
+  )
+  return stdout.split('\n').filter(line => line.length > 0)
+}
+
+function getEnvironmentVariables() {
+  const token = core.getInput('GITHUB_TOKEN')
+  if (!token) {
+    throw Error('GITHUB_TOKEN is required')
   }
+
+  const allAffectedTag = core.getInput('ALL_AFFECTED_TAG')
+  const labelPrefix = JSON.parse(
+    core.getInput('LABEL_PREFIX_DEFINITIONS')
+  ) as Record<string, LabelPrefixDefinition>
+  const projectTypeAbbreviations = JSON.parse(
+    core.getInput('PROJECT_TYPE_ABBREVIATIONS')
+  ) as Record<string, string>
+  const nxHead = core.getInput('NX_HEAD')
+  const nxBase = core.getInput('NX_BASE')
+
+  return {
+    nxHead,
+    nxBase,
+    token,
+    projectTypeAbbreviations,
+    labelPrefix,
+    allAffectedTag
+  }
+}
+
+async function collectAffectedTags(
+  allAffectedTag: string,
+  nxBase: string,
+  nxHead: string,
+  projectTypeAbbreviations: Record<string, string>
+): Promise<Set<string>> {
+  const tags = new Set<string>()
+  const projectGraph = await nx.createProjectGraphAsync()
+  const affected = await getAffectedProjects(nxBase, nxHead)
+  const configurations =
+    nx.readProjectsConfigurationFromProjectGraph(projectGraph).projects
+
+  if (affected.length === Object.keys(configurations).length) {
+    tags.add(allAffectedTag)
+  } else {
+    for (const project of affected) {
+      const config = configurations[project]
+      config.tags.forEach(tag => {
+        const [tagPrefix, tagSuffix] = tag.split(':')
+        if (projectTypeAbbreviations[tagPrefix]) {
+          tags.add(`${tagPrefix}:${tagSuffix}`)
+        } else {
+          tags.add(tag)
+        }
+      })
+      tags.add(`${projectTypeAbbreviations[config.projectType]}:${config.name}`)
+    }
+  }
+  return tags
+}
+
+async function fetchRepositoryLabels(octokit: Octokit): Promise<Set<string>> {
+  const repositoryLabels = new Set<string>()
+  let hasMorePages = true
+  let page = 1
+
+  while (hasMorePages) {
+    const result = await octokit.rest.issues.listLabelsForRepo({
+      owner: 'gorilainvest',
+      repo: 'securities',
+      per_page: 100,
+      page
+    })
+    result.data.map(label => repositoryLabels.add(label.name))
+    hasMorePages = result.data.length === 100
+    page++
+  }
+
+  return repositoryLabels
+}
+
+async function createMissingLabels(
+  octokit: Octokit,
+  tags: Set<string>,
+  existingLabels: Set<string>,
+  labelPrefix: Record<string, LabelPrefixDefinition>
+) {
+  for (const tag of tags) {
+    if (!existingLabels.has(tag)) {
+      const [tagPrefix] = tag.split(':')
+      if (labelPrefix[tagPrefix]) {
+        console.log(`Creating custom definition for label ${tag}`)
+        await octokit.rest.issues.createLabel({
+          owner: 'gorilainvest',
+          repo: 'securities',
+          name: tag,
+          color: labelPrefix[tagPrefix].color,
+          description: labelPrefix[tagPrefix].description
+        })
+      }
+    }
+  }
+}
+
+export async function run() {
+  const {
+    nxBase,
+    nxHead,
+    token,
+    allAffectedTag,
+    projectTypeAbbreviations,
+    labelPrefix
+  } = getEnvironmentVariables()
+  const octokit = github.getOctokit(token)
+
+  const affectedTags = await collectAffectedTags(
+    allAffectedTag,
+    nxBase,
+    nxHead,
+    projectTypeAbbreviations
+  )
+  const repositoryLabels = await fetchRepositoryLabels(octokit)
+
+  await createMissingLabels(
+    octokit,
+    affectedTags,
+    repositoryLabels,
+    labelPrefix
+  )
+
+  const { owner, repo, number } = github.context.issue
+  await octokit.rest.issues.addLabels({
+    owner,
+    repo,
+    issue_number: number,
+    labels: Array.from(affectedTags)
+  })
 }
